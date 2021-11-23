9f101d3cdaf7504967c0423bf44d626c7863858a
Sean Gilleran <sgilleran@gmail.com>
2021-11-22 23:28:54 -0800

    Moved tidy.conf to scripts folder

--- scripts/render.sh
+++ scripts/render.sh
@@ -4,7 +4,7 @@ rm content/.html/*
 
 for filename in content/*.md; do
     pandoc -o /tmp/content.html "$filename" --ascii &&
-    tidy -config tidy.conf /tmp/content.html > content/.html/$(basename "${filename%.*}").html &&
+    tidy -config scripts/tidy.conf /tmp/content.html > content/.html/$(basename "${filename%.*}").html &&
     sed '/\.\.\./q' "$filename" > content/.html/$(basename "${filename%.*}").yaml &&
     rm /tmp/content.html
 done

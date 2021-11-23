18e55d8658af77f882327bafe643a7b1af65b2c9
Sean Gilleran <sgilleran@gmail.com>
2021-11-23 02:42:59 -0800

    Added autolink_bare_uris to pandoc call

--- scripts/render.sh
+++ scripts/render.sh
@@ -3,7 +3,7 @@
 rm content/.html/*
 
 for filename in content/*.md; do
-    pandoc -o /tmp/content.html "$filename" --ascii &&
+    pandoc -o /tmp/content.html "$filename" -f markdown+autolink_bare_uris --ascii &&
     tidy -config scripts/tidy.conf /tmp/content.html > content/.html/$(basename "${filename%.*}").html &&
     sed '/\.\.\./q' "$filename" > content/.html/$(basename "${filename%.*}").yaml &&
     rm /tmp/content.html
